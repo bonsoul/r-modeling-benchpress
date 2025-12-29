ce3d57a8c496866e0bc8d334cef4c0e36fbdd0f4
bonsoul <bonsoul24@gmail.com>
2025-12-29 12:25:05 +0300

    Script for Visuals

--- Visuals.R
+++ Visuals.R
@@ -4,7 +4,8 @@ library(sjPlot)
 library(likert)
 library(lme4)
 library(lmerTest)
-
+library(psych)
+library(psychTools)
 
 view_df(Wage, show.frq = T, show.prc = T, show.na = T)
 
@@ -12,3 +13,56 @@ view_df(Wage, show.frq = T, show.prc = T, show.na = T)
 
 Wage %>%
   plot_frq(education)
+
+P <- Wage %>%
+  group_by(race) %>%
+  plot_frq(education) %>%
+  plot_grid()
+
+
+save_plot(
+  filename = "race_vs_education.jpg",
+  fig = P, width = 30, height  =19
+)
+
+plot_grpfrq(
+  var.cnt = Wage$education,
+  var.grp = Wage$jobclass,
+  
+)
+
+
+
+plot_xtab(
+  x = Wage$education,
+  grp = Wage$jobclass,
+  margin = "row",
+  bar.pos = "stack",
+  show.summary = TRUE,
+  coord.flip = TRUE
+)
+
+tab_xtab(
+  var.row = Wage$education,
+  var.col = Wage$jobclass,
+  show.row.prc = T
+)
+
+
+
+
+Wage %>%
+  group_by(jobclass) %>%
+  plot_frq(
+    wage, 
+    type = "histogram",
+    show.mean = TRUE,
+    normal.curve = TRUE
+  ) %>%
+  plot_grid()
+
+
+data(pisaitems)
+
+head(pisaitems)
+
